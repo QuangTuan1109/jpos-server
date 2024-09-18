cdc74850cc2ccfed446c59d97a9031932d390c1f
QuangTuan1109 <74809953+QuangTuan1109@users.noreply.github.com>
2024-09-18 14:54:06 +0700

    Initial commit

---
+++ src/main/java/com/examples/jpos_springboot/JposSpringbootApplication.java
@@ -0,0 +1,22 @@
+package com.examples.jpos_springboot;
+
+import org.jpos.q2.Q2;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JposSpringbootApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JposSpringbootApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Q2 q2 = new Q2();
+		q2.start();
+//		Thread thread = new Thread(q2);
+//		thread.start();
+	}
+}
